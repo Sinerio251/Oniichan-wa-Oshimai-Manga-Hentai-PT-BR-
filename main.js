f3175020ce336dc85088b2b8551a494c902dc0a8
Sinerio251 <aramalo781@gmail.com>
2025-04-25 13:41:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,12 @@ async function getMangaDetails(mangaId) {
              "https://github.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/blob/main/Capítulo 01/image_07.jpg",
              "https://github.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/blob/main/Capítulo 01/image_08.jpg",
              "https://github.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/blob/main/Capítulo 01/image_09.jpg",
-             "https://github.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/blob/main/Capítulo 01/image_10.jpg", 
+             "https://github.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/blob/main/Capítulo 01/image_10.jpg",
+             "https://github.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/blob/main/Capítulo 01/image_11.jpg", 
+             "https://github.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/blob/main/Capítulo 01/image_12.jpg",
+             "https://github.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/blob/main/Capítulo 01/image_13.jpg",
+             "https://github.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/blob/main/Capítulo 01/image_14.jpg",
+             "https://github.com/Sinerio251/Oniichan-wa-Oshimai-Manga-Hentai-PT-BR-/blob/main/Capítulo 01/image_15.jpg", 
            ]
          }
        ]

91803a3edf1a29cabb3ecf1b3c294d7310d79cce
Sinerio251 <aramalo781@gmail.com>
2025-04-25 13:20:19 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+async function getMangaDetails(mangaId) {
+     return {
+       title: "Oniichan-wa-Oshimai-Manga-Hentai-PT-BR",
+       author: "Seu Nome",
+       chapters: [
+         {
+           id: "Capítulo 01",
+           title: "Capítulo 1",
+           date: "2024-01-01",
+           urls: [
+             "cap1/pagina1.jpg",
+             "cap1/pagina2.jpg"
+           ]
+         }
+       ]
+     };
+}
